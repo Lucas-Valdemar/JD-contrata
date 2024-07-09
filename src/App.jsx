997d985b8e5526d7ecf776666eea250500175b77
Lucas Valdemar <lucas_valdemar@outlook.com>
2024-07-08 22:38:33 -0300

    separacao de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import AvailablePosition from "./components/AvailablePosition";
+import EmployessGraph from "./components/EmployessGraph";
 import Layout from "./components/Layout";
+import TitleNotification from "./components/TitleNotification";
 
 const App = () => {
   return (
     <Layout>
       <div>oi</div>
+      <AvailablePosition />
+      <EmployessGraph />
+      <TitleNotification />
     </Layout>
   );
 };
---
+++ src/components/AvailablePosition/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./style.module.css";
+
+const AvailablePosition = () => {
+  return (
+    <div>
+      <h1 className={style.exemplo}>Deivid Mesquita</h1>
+    </div>
+  );
+};
+
+export default AvailablePosition;
